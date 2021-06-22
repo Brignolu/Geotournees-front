341ffd86a29974455a0e65e62d4669a15ccc10b8
brignolu <dontcrenie@yahoo.fr>
2021-06-22 09:00:37 +0200

    Formulaire ajout abonné corrigé

--- src/components/Personne.vue
+++ src/components/Personne.vue
@@ -4,7 +4,7 @@
       <b-col>
         <b-card>
           <b-form>
-            <b-form-group label="Numéro d'abonné :">
+            <b-form-group label="Informations abonné :">
               <b-form-input
 
                   v-model="form.numero_abo"
@@ -12,8 +12,8 @@
                   placeholder="Numéro abonné"
                   required
               ></b-form-input>
-            </b-form-group>
-            <b-form-group label="Transmetteur :">
+
+
               <b-form-input
 
                   v-model="form.transmetteur"
@@ -21,16 +21,70 @@
                   placeholder="Transmetteur"
                   required
               ></b-form-input>
-            </b-form-group>
-            <b-form-group label="Identifiant WBB :">
-              <b-form-input
 
+
+              <b-form-input
                   v-model="form.identifiant_wbb"
                   type="text"
                   placeholder="Identifiant WBB"
                   required
               ></b-form-input>
+            </b-form-group>
 
+            <b-form-group label="Informations personnelles :">
+              <b-form-input
+
+                  v-model="form.nom"
+                  type="text"
+                  placeholder="Nom"
+                  required
+              ></b-form-input>
+
+              <b-form-input
+
+                  v-model="form.prenom"
+                  type="text"
+                  placeholder="Prénom"
+                  required
+              ></b-form-input>
+
+              <b-form-input
+
+                  v-model="form.numtel"
+                  type="text"
+                  placeholder="N° téléphone"
+                  required
+              ></b-form-input>
+            </b-form-group>
+
+            <b-form-group label="Adresse :">
+              <b-form-input
+                  v-model="form.numero"
+                  type="text"
+                  placeholder="Numéro de voie"
+                  required
+              ></b-form-input>
+
+              <b-form-input
+                  v-model="form.rue"
+                  type="text"
+                  placeholder="Rue"
+                  required
+              ></b-form-input>
+
+              <b-form-input
+                  v-model="form.codepostal"
+                  type="text"
+                  placeholder="CP"
+                  required
+              ></b-form-input>
+
+              <b-form-input
+                  v-model="form.ville"
+                  type="text"
+                  placeholder="Ville"
+                  required
+              ></b-form-input>
             </b-form-group>
 
             <div>
@@ -71,6 +125,48 @@ export default {
         this.$router.push({name: 'intervention'});
 
       }).catch(err => console.log(err))
+
+/*
+      axios.post('http://localhost:3000/create/intervention', {
+            numero_abo: this.form.numero_abo,
+            transmetteur: this.form.transmetteur,
+            identifiant_wbb: this.form.identifiant_wbb,
+            personneId: this.form.personneId,
+          }
+      )
+          .then(function (response) {
+            console.log(response.status);
+            console.log(response);
+            if (response.status === 201) {
+              return response.data
+            }
+          }).then(() => {
+
+        this.$store.commit("messagecreate", "Abonné Créé !")
+        this.$router.push({name: 'intervention'});
+
+      }).catch(err => console.log(err))
+
+      axios.post('http://localhost:3000/create/intervention', {
+            numero_abo: this.form.numero_abo,
+            transmetteur: this.form.transmetteur,
+            identifiant_wbb: this.form.identifiant_wbb,
+            personneId: this.form.personneId,
+          }
+      )
+          .then(function (response) {
+            console.log(response.status);
+            console.log(response);
+            if (response.status === 201) {
+              return response.data
+            }
+          }).then(() => {
+
+        this.$store.commit("messagecreate", "Abonné Créé !")
+        this.$router.push({name: 'intervention'});
+
+      }).catch(err => console.log(err))
+      */
     }
   },
   beforeMount() {
@@ -80,11 +176,15 @@ export default {
   data() {
     return {
       form: {
-        numero_abo: 0,
-        transmetteur: 0,
-        identifiant_wbb: 0,
+        numero_abo: "",
+        transmetteur: "",
+        identifiant_wbb: "",
         personneId: 0,
-
+        nom: "",
+        prenom:"" ,
+        numtel:"",
+        abonneId: "",
+        adresseId:""
       },
     }
   }
